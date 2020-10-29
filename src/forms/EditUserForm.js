35b4735159175030882ffe000acef25d886ab00a
sedx876 <swatkins876@gmail.com>
2020-10-29 09:42:31 -0400

    code cleanup

--- src/forms/EditUserForm.js
+++ src/forms/EditUserForm.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const EditUserForm = (props) => {
   const [user, setUser] = useState(props.currentUser)
 
   const handleInputChange = (event) => {
     const { name, value } = event.target
-
     setUser({ ...user, [name]: value })
   }
+
+  useEffect(() => {
+    setUser(props.currentUser)
+  }, [props])
 
   return (
     <form
