bc31710afde31e3698f0c0015346add08ef805b1
sedx876 <swatkins876@gmail.com>
2020-10-29 09:09:13 -0400

    added tables/usertable.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react'
+import UserTable from './tables/UserTable'
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
